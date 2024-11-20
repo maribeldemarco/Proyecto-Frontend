8314d0dd1f682ed5c74ec511460693dfeb0a1e78
Linda M De Marco <maribeldemarco@gmail.com>
2024-11-20 20:38:10 -0300

    se suben estilos del componente para agregar productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ComponenteAgregarComponent } from './components/componente-agregar/componente-agregar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'agregarproducto', component:ComponenteAgregarComponent}
+];
