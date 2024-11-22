8dee66b948e14a6120380471ddd428bcfa551b74
Linda M De Marco <maribeldemarco@gmail.com>
2024-11-22 00:29:22 -0300

    se agregan mensajes de error al formulario

--- src/app/components/componente-agregar/componente-agregar.component.ts
+++ src/app/components/componente-agregar/componente-agregar.component.ts
@@ -1,35 +1,37 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Producto } from '../../models/agregar.model';
 import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common'; 
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-componente-agregar',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule], 
   templateUrl: './componente-agregar.component.html',
   styleUrls: ['./componente-agregar.component.css']
 })
 export class ComponenteAgregarComponent {
-
   miFormulario: FormGroup;
   producto?: Producto;
+  mensaje: string | null = null;
+  exito: boolean = false;
 
   constructor(private _fb: FormBuilder, private _apiService: ApiService) {
-  this.miFormulario = this._fb.group({
-  id: 0,
-  Nombre: ['', Validators.required],
-  Marca: ['', Validators.required],
-  Stock: ['', [Validators.required, Validators.min(0)]], 
-  Perece: ['', [Validators.required, Validators.min(0)]],
-  Fecha_Vencimiento: ['', Validators.required], 
-  ProveedorID: ['', [Validators.required, Validators.min(1)]], 
-  CategoriaSubcategoriaID: ['', [Validators.required, Validators.min(1)]], 
-});
+    this.miFormulario = this._fb.group({
+      id: 0,
+      Nombre: ['', Validators.required],
+      Marca: ['', Validators.required],
+      Stock: ['', [Validators.required, Validators.min(0)]],
+      Perece: ['', [Validators.required, Validators.min(0)]],
+      Fecha_Vencimiento: ['', Validators.required],
+      ProveedorID: ['', [Validators.required, Validators.min(1)]],
+      CategoriaSubcategoriaID: ['', [Validators.required, Validators.min(1)]]
+    });
   }
 
   enviar() {
-    // Verifica si el formulario es válido
     if (this.miFormulario.valid) {
       this.producto = {
         Nombre: this.miFormulario.value.Nombre,
@@ -43,14 +45,19 @@ export class ComponenteAgregarComponent {
 
       this._apiService.postProducto(this.producto).subscribe({
         next: (respuesta) => {
-          console.log('Producto agregado correctamente:', respuesta);
-          this.miFormulario.reset();  
+          this.mensaje = 'Producto agregado correctamente';
+          this.exito = true;
+          this.miFormulario.reset();
         },
         error: (error) => {
+          this.mensaje = 'Error al agregar el producto, verifique la información';
+          this.exito = false;
           console.error('Error al agregar el producto:', error);
         }
       });
     } else {
+      this.mensaje = 'Formulario incompleto';
+      this.exito = false;
       console.error('Formulario invalido', this.miFormulario.errors);
     }
   }
