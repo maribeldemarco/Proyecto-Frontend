f1f7515825c456d5e73192e0b7323dda9cc33cb7
Christian Gabor <chrisgabor95@gmail.com>
2024-11-22 03:24:22 -0300

    home lindo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { FilterComponent } from './components/filter/filter.component';
 import { HomeComponent } from './components/home/home.component';
-import { AgregarComponent } from './components/agregar/agregar.component';
+// import { AgregarComponent } from './components/agregar/agregar.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'inventario',component: FilterComponent},
-  { path: 'agregar-producto', component: AgregarComponent },
+  { path: 'inventario', component: FilterComponent},
+  // { path: 'agregar-producto', component: AgregarComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
