dbf4c5bf7daf5ed2a76736498de186ffc450729d
Christian Gabor <chrisgabor95@gmail.com>
2024-11-22 15:38:17 -0300

    eliminacionde archivos innesesarios

--- src/app/models/actualizar.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Producto {
-    "ProveedorID": number,
-        "CategoriaSubcategoriaID": number,
-        "Nombre": string,
-        "Marca": string,
-        "Stock": number,
-        "Perece": number,
-        "Fecha_Vencimiento": Date,
-    }
