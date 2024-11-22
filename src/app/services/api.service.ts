f1a3bc6fe3d6db60041ed0b67ca73153ece949d0
Guada <74994966+GuadalupeSirio@users.noreply.github.com>
2024-11-22 01:27:31 -0300

    Inventory (#2)
    * eliminacionde .gitignore innecesario
    
    * Add new component tabla-principal
    
    * Add changes to implementation
    
    * Update estructure
    
    ---------
    
    Co-authored-by: Christian Gabor <cgabordellicarri@frba.utn.edu.ar>

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProducto } from '../models/producto.model';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class ApiService {
+    private url = 'http://localhost:3000/productos';
+
+    constructor(private _http:HttpClient) {}
+
+    public getProducts():Observable<IProducto[]> {
+        return this._http.get<IProducto[]>(this.url);
+    }
+
+    deleteProduct(productId: number):Observable<IProducto>{
+      return this._http.delete<IProducto>(`${this.url}/${productId}`);
+    }
+}
